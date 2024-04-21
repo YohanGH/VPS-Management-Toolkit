97baffa3a167f3f46bf2b37abc3d0816848099f8
YohanGH <YohanGH@proton.me>
2024-04-21 14:52:14 +0200

    Modification file create error management

--- scripts/create_restricted_users.sh
+++ scripts/create_restricted_users.sh
@@ -6,7 +6,7 @@
 #    By: YohanGH <YohanGH@proton.me>                    //    ''     Code      #
 #                                                      (|     | )              #
 #    Created: 2024/04/15 20:47:51 by YohanGH           '__   _/_               #
-#    Updated: 2024/04/21 14:16:29 by YohanGH          (___)=(___)              #
+#    Updated: 2024/04/21 14:51:17 by YohanGH          (___)=(___)              #
 #                                                                              #
 # **************************************************************************** #
 
@@ -33,24 +33,34 @@ if [ ! -f "$filename" ]; then
     exit 1
 fi
 
+# Chemin du fichier journal
+logfile="/var/log/user_creation.log"
+
 # Création de plusieurs utilisateurs à partir d'une liste
 while IFS= read -r username
 do
-    # Vérifie si l'utilisateur existe déjà
-    if id "$username" &>/dev/null; then
-        echo "L'utilisateur '$username' existe déjà." >&2
+	# Validation du nom d'utilisateur
+    if [[ ! "$username" =~ ^[a-zA-Z0-9_]+$ ]]; then
+        echo "Nom d'utilisateur '$username' invalide. Les noms d'utilisateur ne peuvent contenir que des lettres, des chiffres et des underscores." | tee -a "$logfile"
         continue
     fi
 
-    # Création de l'utilisateur avec un shell standard et sans ajout au groupe sudo
-    useradd -m -s /bin/bash -G users "$username"
+	 # Vérifie si l'utilisateur existe déjà
+    if id "$username" &>/dev/null; then
+        echo "L'utilisateur '$username' existe déjà." | tee -a "$logfile"
+        continue
+    fi
 
-    # Définition d'un mot de passe temporaire et forçage de la modification du mot de passe à la première connexion
-    echo "Définissez un mot de passe temporaire pour $username:"
-    passwd "$username"
-    passwd -e "$username"
+	# Création de l'utilisateur avec un shell standard et sans ajout au groupe sudo
+    if useradd -m -s /bin/bash -G users "$username"; then
+		# Forcer le changement de mot de passe à la première connexion
+		passwd -de "$username"
 
-    # Confirmation de la création de l'utilisateur
-    echo "Utilisateur '$username' créé avec succès avec des droits limités."
-    echo "L'utilisateur devra changer son mot de passe lors de la prochaine connexion."
+    echo "Utilisateur '$username' créé avec succès avec des droits limités." | tee -a "$logfile"
+        echo "Mot de passe temporaire généré pour '$username'. L'utilisateur doit le changer à la première connexion." | tee -a "$logfile"
+    else
+        echo "Erreur lors de la création de l'utilisateur '$username'." | tee -a "$logfile"
+    fi
 done < "$filename"
+
+echo "Opération terminée. Vérifiez $logfile pour le journal détaillé des opérations."
